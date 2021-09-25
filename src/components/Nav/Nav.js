3ebf1765a5df6c0ec9a949214c0eb89c9e51278c
Moddoo <>
2021-09-25 00:00:02 -0500

    nav/starter

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,10 @@
+import {Title} from './Nav.styles'
+function Nav() {
+    return (
+        <Title>
+            Nav Bar
+        </Title>
+    )
+}
+
+export default Nav
---
+++ src/components/Nav/Nav.styles.js
@@ -0,0 +1,7 @@
+import styled  from "styled-components";
+
+export const Title = styled.h1`
+    font-family: "Inter";
+    font-size: 50px;
+    color: black;
+`
