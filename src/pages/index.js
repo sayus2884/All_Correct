d7f7749ca8ba8e5c934895dd7219db336df2a8f8
Mprystal <mjprystal@gmail.com>
2021-09-27 15:35:23 -0400

    add active section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Nav from "../components/Nav/Nav.js";
 import Footer from "../components/Footer/Footer";
+import Active from "../components/Active/Active.js";
 
 export default function Home() {
   return (
     <>
       <Nav lang={true} />
+      <Active />
       <Footer />
     </>
   );
