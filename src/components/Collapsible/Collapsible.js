004c163c8c4a91f9ce7ce0ae54eb5a82b824c089
sayus2884 <joniah2884@gmail.com>
2021-09-28 13:12:07 -0700

    Update Collapsible.js
    remove unnecessary handler

--- src/components/Collapsible/Collapsible.js
+++ src/components/Collapsible/Collapsible.js
@@ -2,11 +2,6 @@ import { Container } from "./Collapsible.styles";
 
 function Collapsible({ children }) {
 
-  const handleClick = (event) => {
-    event.preventDefault();
-    console.log('handling click');
-  }
-
   return (
     <Container>
       {children}
