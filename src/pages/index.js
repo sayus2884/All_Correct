4992e876391be2ede7076827f7afbaa2058f495a
Moddoo <>
2021-09-29 11:10:24 -0500

    review/done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Nav from "../components/Nav/Nav.js";
+import Reviews from "../components/Reviews/Reviews.js";
 import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
     <>
       <Nav lang={true} />
+      <Reviews />
       <Footer />
     </>
   );
