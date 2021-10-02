72f28ca60f5a5051bf6ce1541e7e212efb188092
Moddoo <>
2021-10-02 08:13:34 -0500

    gamesgrid/animation/menu/load

--- src/components/Games/Games.styles.js
+++ src/components/Games/Games.styles.js
@@ -22,7 +22,7 @@ export const GamesGrid = styled.div`
   grid-template-columns: repeat(3, auto);
   grid-auto-rows: auto;
   overflow: hidden;
-  animation: ${(props) => (props.show ? "" : "shrink .5s alternate")};
+  animation: ${(props) => (props.show ? "" : "shrink .5s ease-out forwards")};
 
   @keyframes shrink {
     0% {
