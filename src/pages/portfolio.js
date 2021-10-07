68061dbf84ce3f68233a492bda084588ed81331b
bakagai <>
2021-10-06 17:00:13 -0700

    refactor: remove unnecessary imports

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,10 +1,6 @@
 import Nav from "../components/Nav/Nav.js";
-import GameCarousel from "../components/GameCarousel/GameCarousel.js";
-import Services from "../components/Services/Services.js";
-import Active from "../components/Active/Active.js";
 import Games from "../components/Games/Games.js";
 import Reviews from "../components/Reviews/Reviews.js";
-import Companies from "../components/Companies/Companies";
 import Footer from "../components/Footer/Footer.js";
 import { useState } from "react";
 
