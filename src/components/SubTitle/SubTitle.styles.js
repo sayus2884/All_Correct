f1b8271ff08dbc963498e71d8e457eae053bb5fd
bakagai <>
2021-10-13 17:54:34 -0700

    feature: add SubTitle component

---
+++ src/components/SubTitle/SubTitle.styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Header = styled.h3`
+  background-color: ${(props) => props.theme.fonts.inter};
+  font-style: normal;
+  font-weight: bold;
+  font-size: 32px;
+  line-height: 30px;
+  letter-spacing: -0.01em;
+`;
