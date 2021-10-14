a68c84a99446438c6d89ece8a314d3e40bb8a214
bakagai <>
2021-10-13 17:43:46 -0700

    feature: add title component

---
+++ src/components/Title/Title.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  background-color: ${(props) => props.theme.fonts.inter};
+  font-style: normal;
+  font-weight: bold;
+  font-size: 52px;
+  line-height: 63px;
+  letter-spacing: -0.01em;
+
+  &.section-title {
+    margin-bottom: 80px;
+  }
+`;
