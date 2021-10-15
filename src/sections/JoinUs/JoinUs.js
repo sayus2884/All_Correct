3881ebdb2ad9377485d05799423d75c7b108d8e4
bakagai <>
2021-10-14 21:18:18 -0700

    refactor: join us section

--- src/sections/JoinUs/JoinUs.js
+++ src/sections/JoinUs/JoinUs.js
@@ -1,16 +1,22 @@
-import { Container, Title, Text, LinksContainer, Link } from "./JoinUs.styles";
+import { Section, Title, Text, LinksContainer } from "./JoinUs.styles";
+
+import Link from "../../components/Link/Link.js";
 
 function JoinUs({ title, description }) {
   return (
-    <Container>
-      <Title>{title}</Title>
+    <Section>
+      <Title className="section-title">{title}</Title>
 
       {description && <Text>{description}</Text>}
       <LinksContainer>
-        <Link>For linguists &#10230;</Link>
-        <Link>For artists &#10230;</Link>
+        <Link href="#" scroll={false}>
+          <Title>For linguists &#10230;</Title>
+        </Link>
+        <Link href="#" scroll={false}>
+          <Title>For artists &#10230;</Title>
+        </Link>
       </LinksContainer>
-    </Container>
+    </Section>
   );
 }
 
