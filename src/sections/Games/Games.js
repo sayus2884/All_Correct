a964b93923b5217ac621d36a52b27935f854a3e3
unknown <franjseco@gmail.com>
2021-10-17 18:04:14 +0200

    fix: working on filter

--- src/sections/Games/Games.js
+++ src/sections/Games/Games.js
@@ -15,20 +15,58 @@ function Games() {
   const [showAll, setShow] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
+  const [indexShow, setIndexShow] = useState(6);
+
   function toggle(index) {
     isOpen === index ? setIsOpen(false) : setIsOpen(index);
   }
 
+  function loadMore() {
+    if ((indexShow < allGames.length) && (showAll === false)) {
+      setIndexShow(indexShow + 6);
+      setShow(false);
+    } else if ((indexShow >= allGames.length) && showAll === false) {
+      setShow(true);
+    } else {
+      setIndexShow(6);
+    }
+  }
+
+  const [platform, setPlatform] = useState('All');
+  const [genre, setGenre] = useState('All');
+  const [type, setType] = useState('All');
+
+  const data = () => {
+    if (platform === 'All' && genre === 'All' && type === 'All') {
+      return allGames;
+    } else if (platform !== 'All' || genre !== 'All' || type !== 'All') {
+      console.log(platform, genre, type);
+      const newElement = allGames.filter(
+        element =>
+          element.platforms.includes(platform) ||
+          element.genre.includes(genre) ||
+          element.services.includes(type));
+      return newElement;
+    }
+  }
+
+  // const data = (platform === 'All' && genre === 'All' || type === 'All') && allGames;
+
+
+
+  console.log(allGames);
+
   return (
     <Section>
       <MenuList>
-        <DropDown title="Platform">
+        <DropDown title="Platform" id='platform' onItemSelected={(e) => setPlatform(e)}>
           <DropDownItem value={"All"} />
           <DropDownItem value={"Mobile"} />
           <DropDownItem value={"PC"} />
+          <DropDownItem value={"Nintendo Switch"} />
           <DropDownItem value={"Cross-platform"} />
         </DropDown>
-        <DropDown title="Genre">
+        <DropDown title="Genre" id='genre' onItemSelected={(e) => setGenre(e)}>
           <DropDownItem value={"All"} />
           <DropDownItem value={"Match 3"} />
           <DropDownItem value={"Strategy"} />
@@ -42,19 +80,19 @@ function Games() {
           <DropDownItem value={"City"} />
           <DropDownItem value={"Puzzle"} />
         </DropDown>
-        <DropDown title="Type of project">
-          <DropDownItem value={"RPG"} />
-          <DropDownItem value={"MMORPG"} />
-          <DropDownItem value={"City builder"} />
-          <DropDownItem value={"Puzzle"} />
+        <DropDown title="Type of project" id='type' onItemSelected={(e) => setType(e)}>
+          <DropDownItem value={"All"} />
+          <DropDownItem value={"Game localization"} />
+          <DropDownItem value={"Localization testing"} />
+          <DropDownItem value={"Voiceovers"} />
         </DropDown>
       </MenuList>
       <GamesGrid show={showAll}>
-        {allGames.map((e, i) => (
+        {data().filter((element, i) => i < indexShow).map((e, i) => (
           <GameCard key={i} game={e} height={300} />
         ))}
       </GamesGrid>
-      <Text className="button blue" onClick={() => setShow(!showAll)}>
+      <Text className="button blue" onClick={loadMore}>
         {showAll ? <span>Load less &#8593;</span> : "Load more ↓"}
       </Text>
     </Section>
